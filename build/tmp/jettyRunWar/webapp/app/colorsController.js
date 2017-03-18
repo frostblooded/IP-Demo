081533ddefbdd123ab9e26531af7d04f3f5e61c9
frostblooded <frostblooded@yahoo.com>
2017-03-18 16:30:20 +0200

    Update gitignore

--- build/tmp/jettyRunWar/webapp/app/colorsController.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('restApp', ['infinite-scroll']);
-
-app.controller('ColorsController', ['$scope', '$http', function($scope, $http) {
-	$http.get('api/colors').success(function(res) {
-		$scope.colors = res;
-	});
-}]);
